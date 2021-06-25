1fa3e4891ea4d522542e01f10ba585ee844d1577
Dev Krishn <devkrishn9@gmail.com>
2021-06-25 23:58:14 +0530

    Created model for sign up button

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//----------For Sign In button--------------
 var modal = document.getElementById("mymodal");
 
 // Get the button that opens the modal
@@ -21,4 +22,37 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
  }
+}
+//----------For Sign Up button--------------
+var modal1 = document.getElementById("mymodal2");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("up");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+ }
+}
+
+// For redirecting sign in to sign up------
+var su = document.getElementById('redirect');
+su.onclick=function()
+{
+	modal.style.display="none";
+	modal1.style.display="block";
 }
