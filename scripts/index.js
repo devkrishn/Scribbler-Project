920d6e773cbfa2b54bb5c38cf751c158faa89ab9
Dev Krishn <devkrishn9@gmail.com>
2021-06-26 23:22:08 +0530

    created models for postbuttons

--- scripts/index.js
+++ scripts/index.js
@@ -56,3 +56,29 @@ su.onclick=function()
 	modal.style.display="none";
 	modal1.style.display="block";
 }
+
+//----------For Create Post button--------------
+var modal2 = document.getElementById("mymodal3");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("create");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+ }
+}
